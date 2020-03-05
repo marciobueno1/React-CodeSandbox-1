6d7e3a0d203dd063616a26438eeef865006c159c
Marcio Bueno <marciobueno@gmail.com>
2020-03-04 21:55:04 -0300

    Added semantic-ui-react
    Added simple button example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import * as React from "react";
+import { Button } from "semantic-ui-react";
+
 import "./styles.css";
 
+interface Props {}
+
 export default function App() {
+  const handleOnClick = () => {
+    alert("You clicked!");
+  };
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <Button onClick={handleOnClick} color="olive">
+        Click Here
+      </Button>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { render } from "react-dom";
 
+import 'semantic-ui-css/semantic.min.css'
+
 import App from "./App";
 
 const rootElement = document.getElementById("root");
